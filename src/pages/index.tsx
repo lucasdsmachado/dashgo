87ad574ec0e6ecf6bf68359e58ac5a7e0ba085cb
lucasdsmachado <briandsmachado@gmail.com>
2022-02-08 13:21:12 -0300

    replace chakra inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, FormControl, FormLabel, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Forms/Input";
 
 export default function Home() {
   return (
@@ -16,39 +17,16 @@ export default function Home() {
         borderRadius={8}
         flexDir="column">
         <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name="email"
-              id="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bg="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size='lg'
-            />
-          </FormControl>
-         
-         <FormControl>
-         <FormLabel htmlFor="password">Senha</FormLabel>
-          <Input
-            name="password"
-            id="password"
-            type="password"
-            focusBorderColor="pink.500"
-            bg="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: 'gray.900'
-            }}
-            size='lg'
-          />
-         </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
         </Stack>
-          <Button type="submit" mt={6} colorScheme="pink"> Entrar</Button>
+          <Button
+            type="submit"
+            mt={6}
+            colorScheme="pink"
+            size="lg">
+            Entrar
+          </Button>
       </Flex>
     </Flex>
   )
