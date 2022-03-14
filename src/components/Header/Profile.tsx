9b99bc922698fcf0f0bb4b5bb4e4cfc6da01334e
lucasdsmachado <briandsmachado@gmail.com>
2022-03-14 01:24:43 -0300

    responsive header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-       <Box mr="4" textAlign="right">
-          <Text>Brian Machado</Text>
-          <Text color="gray.300" fontSize="small">
-            brian@gmail.com
-          </Text>
-       </Box>
+       { showProfileData && (
+         <Box mr="4" textAlign="right">
+            <Text>Brian Machado</Text>
+            <Text color="gray.300" fontSize="small">
+              brian@gmail.com
+            </Text>
+         </Box>
+       )}
        <Avatar size="md" name="Brian Machado" src="https://github.com/lucasdsmachado.png"/>
      </Flex>
   );
