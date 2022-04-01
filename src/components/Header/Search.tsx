ed4923ab3435c5b2f46575d5002cf72db1af72b7
lucasdsmachado <briandsmachado@gmail.com>
2022-04-01 02:45:57 -0300

    add ref to search input

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,7 +1,11 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function Search() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  console.log(searchInputRef.current.value);
+  
   return (
     <Flex
       as="label"
@@ -23,7 +27,9 @@ export function Search() {
         mr="4"
         placeholder="Buscar na plataforma"
         _placeholder={{color:
-        'gray.400'}} 
+          'gray.400'
+        }}
+        ref={searchInputRef}
       />
       <Icon as={RiSearchLine} fontSize="20" />
     </Flex>
