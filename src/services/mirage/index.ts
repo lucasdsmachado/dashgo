b71143f59641c35654c2457f585f48675b17bdc3
lucasdsmachado <briandsmachado@gmail.com>
2022-04-03 23:18:38 -0300

    configure miragejs

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,51 @@
+import { createServer, Factory, Model } from 'miragejs';
+import faker from '@withshepherd/faker';
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({})
+    },
+
+
+// gerar dados em massa
+    factories: {
+      user: Factory.extend({
+        name(i) {
+          return `User ${i + 1}`
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      })
+    },
+
+  // criar dados assim que o mirage é iniciado
+
+    seeds(server) {
+      server.createList('user', 10);
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    }
+  })
+
+  return server;
+}
