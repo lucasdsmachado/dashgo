3683826dddc5ae542c4f819be3be2e65a9c2d42a
lucasdsmachado <briandsmachado@gmail.com>
2022-04-05 20:01:44 -0300

    sinalize refetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -43,7 +43,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
             <Link href='/users/create' passHref>
               <Button
