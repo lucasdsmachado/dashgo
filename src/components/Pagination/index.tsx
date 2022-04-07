3df34ccc207d6feb66ead0884ff375616e47e32c
lucasdsmachado <briandsmachado@gmail.com>
2022-04-07 02:56:56 -0300

    add change page function

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -24,7 +24,6 @@ export function Pagination({
   currentPage = 1,
   onPageChange,
 }: PaginationProps) {
-
   const lastPage = Math.ceil(totalCountOfRegisters / registersPerPage);
   const previousPages = currentPage > 1
     ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
@@ -49,27 +48,27 @@ export function Pagination({
 
         {currentPage > (1 + siblingsCount) && (
           <>
-            <PaginationItem number={1} />
+            <PaginationItem onPageChange={onPageChange} number={1} />
             {currentPage > (2 + siblingsCount) &&
               <Text color="gray.300" textAlign="center" width="8">...</Text>}
           </>
         )}
 
         {previousPages.length > 0 && previousPages.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         })}
 
-        <PaginationItem number={currentPage} isCurrent />
+        <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
 
         {nextPages.length > 0 && nextPages.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         })}
 
         {(currentPage + siblingsCount) < lastPage && (
           <>
             {(currentPage + 1 + siblingsCount) < lastPage &&
               <Text color="gray.300" textAlign="center" width="8">...</Text>}
-            <PaginationItem number={lastPage} />
+            <PaginationItem onPageChange={onPageChange} number={lastPage} />
           </>
         )}
 
